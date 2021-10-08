7bd6c8473080edca29ee36ed9016773831a98f93
Josh Nelson <jmarknel@gmail.com>
2021-10-08 01:01:21 -0500

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,29 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '600px',
+      // => @media (min-width: 600px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
 }
